fbaea5b89d16ad6a2446255d2c523359b8b2095d
ceglab <52042219+ceglab@users.noreply.github.com>
2019-12-23 20:14:55 +0530

    Create orthoQC.sh

---
+++ scripts/orthoQC.sh
@@ -0,0 +1,4 @@
+for spe in ptroglodytes ppaniscus ccapucinus capalliatus rbieti sbboliviensis ogarnettii capalliatus pcoquereli mfascicularis mleucophaeus nleucogenys ggorilla psimus anancymaae mmulatta cjacchus pabelii catys csyrichta ptephrosceles csabaeus
+do
+perl getOrthos.pl $spe > "$spe".out
+done
